bb39fb3878ee0fcf192745d223b234c9e5bfbeb2
VPlaksin <valeriy905@gmail.com>
2025-04-08 20:18:23 +0300

    my_1st_commit

---
+++ tests/Test_VP.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+
+def test_login():
+    driver = webdriver.Chrome()
+
+    driver.get("https://www.demoblaze.com/")
+    driver.find_element(By.ID, 'login2').click()
+    time.sleep(5)
+    driver.find_element(By.ID, 'loginusername').send_keys("User_test007")
+    driver.find_element(By.ID, 'loginpassword').send_keys("My_pswr_0912")
+    driver.find_element(By.CSS_SELECTOR, 'button[onclick="logIn()"]').click()
+    time.sleep(5)
+    driver.quit()
+
+
+
