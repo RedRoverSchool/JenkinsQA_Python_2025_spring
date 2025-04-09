9b5a2f5c84423265884ca8006caa2dc74f011893
Mykola Stepanets <nik.stepanets@gmail.com>
2025-04-10 01:45:07 +0300

    Removed time.sleep()

--- tests/test_nstepanets.py
+++ tests/test_nstepanets.py
@@ -1,15 +1,17 @@
-import time
-
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 @pytest.fixture(scope="session")
 def driver():
     driver = webdriver.Chrome()
+    driver.maximize_window()
     yield driver
     driver.quit()
+
 
 @pytest.fixture(scope="function")
 def login_page(driver):
@@ -17,29 +19,38 @@ def login_page(driver):
     return driver
 
 
+@pytest.fixture(scope="session")
+def wait(driver):
+    driver_wait = WebDriverWait(driver, 1)
+    return driver_wait
+
+
+
 def test_login_page_opens(login_page):
     header = login_page.find_element(By.XPATH, '//*[@id="login"]/h2')
     assert header.text == "Test login"
+
 
 def test_login_successful(login_page):
     login_page.find_element(By.ID, "username").send_keys("student")
     login_page.find_element(By.ID, "password").send_keys("Password123")
     login_page.find_element(By.XPATH, '//button[@id="submit"]').click()
-    time.sleep(1)
     assert "practicetestautomation.com/logged-in-successfully/" in login_page.current_url
 
-def test_login_with_invalid_password(login_page):
+
+def test_login_with_invalid_password(login_page, wait):
     login_page.find_element(By.ID, "username").send_keys("student")
     login_page.find_element(By.ID, "password").send_keys("incorrectPassword")
     login_page.find_element(By.XPATH, '//button[@id="submit"]').click()
-    time.sleep(1)
     error_message = login_page.find_element(By.XPATH, '//div[@id="error"]')
+    wait.until(EC.visibility_of(error_message), "Error message not displayed")
     assert error_message.text == "Your password is invalid!"
 
-def test_login_with_invalid_username(login_page):
+
+def test_login_with_invalid_username(login_page, wait):
     login_page.find_element(By.ID, "username").send_keys("incorrectUser")
     login_page.find_element(By.ID, "password").send_keys("Password123")
     login_page.find_element(By.XPATH, '//button[@id="submit"]').click()
-    time.sleep(1)
     error_message = login_page.find_element(By.XPATH, '//div[@id="error"]')
+    wait.until(EC.visibility_of(error_message), "Error message not displayed")
     assert error_message.text == "Your username is invalid!"
