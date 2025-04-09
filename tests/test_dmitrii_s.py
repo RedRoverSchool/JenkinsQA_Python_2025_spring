3cc110d7a3230b9408e6c743f985610b864f4134
dsmirnov <mitsmirnov@gmail.com>
2025-04-09 07:54:19 -0230

    Add new test_dmitrii_s.py

--- tests/test_dmitrii_s.py
+++ tests/test_dmitrii_s.py
@@ -45,3 +45,15 @@ def test_error_user_price_item():
     assert price_text == "$9.99", f"Unexpected price: {price_text}"
 
     driver.quit()
+
+def test_error_user_url_check():
+    driver = webdriver.Chrome()
+
+    driver.get("https://www.saucedemo.com/")
+    driver.find_element(By.ID, 'user-name').send_keys("error_user")
+    driver.find_element(By.ID, 'password').send_keys("secret_sauce")
+    driver.find_element(By.NAME, 'login-button').click()
+
+    assert driver.current_url == "https://www.saucedemo.com/inventory.html", "wrong url"
+
+    driver.quit()
