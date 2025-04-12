75fd97c94700b0442f1cf58553271150d4daac88
KhodzhaevN <hodzhaevnail00@gmail.com>
2025-04-12 16:33:50 +0400

    Full

---
+++ tests/test_nail.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+def test_cart_icon_displays_correct_item_count():
+    driver = webdriver.Chrome()
+    driver.get("https://www.saucedemo.com/")
+
+    driver.find_element(By.ID, "user-name").send_keys("standard_user")
+    driver.find_element(By.ID, "password").send_keys("secret_sauce")
+    driver.find_element(By.ID, "login-button").click()
+    time.sleep(2)
+
+      driver.find_element(By.XPATH, "//*[@id='item_4_title_link']/div").click()
+    time.sleep(2)
+
+    driver.find_element(By.XPATH, "//*[@id='add-to-cart']").click()
+
+     cart_badge = driver.find_element(By.XPATH, "//*[@id='shopping_cart_container']/a/span")
+    assert cart_badge.text == "1", f"Ожидалось '1' в корзине, но получено '{cart_badge.text}'"
+
+    driver.quit()
