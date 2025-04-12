cc11d51177eb2030327aa542970ef2376a81cbc5
IvanSoregashi <ivanshiriaev2222@gmail.com>
2025-04-13 00:12:40 +0500

    add my test 2

--- tests/test_ivan_s_test.py
+++ tests/test_ivan_s_test.py
@@ -12,8 +12,8 @@ def test_locked_out_user_error_message(sauce):
     sauce.find_element(By.ID, 'user-name').send_keys("locked_out_user")
     sauce.find_element(By.ID, 'password').send_keys("secret_sauce")
     sauce.find_element(By.NAME, 'login-button').click()
-
     error_message = sauce.find_element(By.XPATH, "//h3[@data-test='error']")
+
     assert error_message.text == "Epic sadface: Sorry, this user has been locked out."
 
 def test_locked_out_user_url_check(sauce):
