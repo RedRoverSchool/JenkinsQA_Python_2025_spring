a635fc882c8c45e27f8d4b425421ae447f125029
aidar3532 <aidar3532@gmail.com>
2025-04-13 22:38:14 +0300

    Practice lesson 1

---
+++ old/test_aidar_saucedemo.py
@@ -0,0 +1,34 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+def test_locked_out_user_error_message():
+    driver = webdriver.Chrome()
+
+    driver.get("https://www.saucedemo.com/")
+    driver.find_element(By.ID, "user-name").send_keys("locked_out_user")
+    time.sleep(1)
+    driver.find_element(By.ID, "password").send_keys("secret_sauce")
+    time.sleep(1)
+    driver.find_element(By.ID, "login-button").click()
+
+    error_message = driver.find_element(By.XPATH, "//h3[@data-test='error']")
+    assert error_message.text == "Epic sadface: Sorry, this user has been locked out."
+
+    driver.quit()
+
+def test_locked_out_user_url_check():
+    driver = webdriver.Chrome()
+
+    driver.get("https://www.saucedemo.com/")
+    driver.find_element(By.ID, "user-name").send_keys("locked_out_user")
+    time.sleep(1)
+    driver.find_element(By.ID, "password").send_keys("secret_sauce")
+    time.sleep(1)
+    driver.find_element(By.ID, "login-button").click()
+
+    assert driver.current_url == "https://www.saucedemo.com/", "wrong url"
+
+    driver.quit()
+
