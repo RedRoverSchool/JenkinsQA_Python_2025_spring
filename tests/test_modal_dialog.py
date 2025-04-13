1b62fd54e8e1fed84737b37c1addf6cacb8a132d
ppashchenkov <ppashchenkov@gmail.com>
2025-04-13 21:55:48 +0300

    Added test_modal_dialog.py

--- tests/test_modal_dialog.py
+++ tests/test_modal_dialog.py
@@ -11,34 +11,41 @@ def modal(driver):
     driver.get("https://demoqa.com/modal-dialogs")
     return driver
 
-def test_small_modal_dialog(modal):
-    sm_text = "This is a small modal. It has very less content"
+@pytest.mark.parametrize(
+    "body_text, id_show, id_button, title",
+    [
+        ("This is a small modal", "showSmallModal", "closeSmallModal", "Small Modal"),
+        ("Lorem Ipsum is simply dummy text of the printing and typesetting industry", "showLargeModal",
+         "closeLargeModal", "Large Modal")
+    ]
+)
+def test_modal_dialog(modal, body_text, id_show, id_button, title):
     waiter5 = WebDriverWait(modal, 5)
-    modal.find_element(By.ID, "showSmallModal").click()
+    modal.find_element(By.ID, id_show).click()
     waiter5.until(EC.presence_of_element_located((By.XPATH, '//div[@role="dialog"]')))
     modal.switch_to.active_element.find_element(By.XPATH, '//div[@role="dialog"]')
-    sm_title = modal.find_element(By.CLASS_NAME, "modal-title").text
-    sm_body = modal.find_element(By.CLASS_NAME, "modal-body").text
-    sm_button = modal.find_element(By.ID, "closeSmallModal")
-    sm_button_text = sm_button.text
-    sm_button.click()
+    m_title = modal.find_element(By.CLASS_NAME, "modal-title").text
+    m_body = modal.find_element(By.CLASS_NAME, "modal-body").text.split(".")
+    m_button = modal.find_element(By.ID, id_button)
+    m_button_text = m_button.text
+    m_button.click()
 
-    assert sm_title == "Small Modal"
-    assert sm_body == sm_text
-    assert sm_button_text == "Close"
+    assert m_title == title
+    assert m_body[0] == body_text
+    assert m_button_text == "Close"
 
-def test_large_modal_dialog(modal):
-    lm_text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-    waiter5 = WebDriverWait(modal, 5)
-    modal.find_element(By.ID, "showLargeModal").click()
-    waiter5.until(EC.presence_of_element_located((By.XPATH, '//div[@role="dialog"]')))
-    modal.switch_to.active_element.find_element(By.XPATH, '//div[@role="dialog"]')
-    lm_title = modal.find_element(By.CLASS_NAME, "modal-title").text
-    lm_body = modal.find_element(By.CLASS_NAME, "modal-body").text
-    lm_button = modal.find_element(By.ID, "closeLargeModal")
-    lm_button_text = lm_button.text
-    lm_button.click()
-
-    assert lm_title == "Large Modal"
-    assert lm_body == lm_text
-    assert lm_button_text == "Close"
+# def test_large_modal_dialog(modal):
+#     lm_text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+#     waiter5 = WebDriverWait(modal, 5)
+#     modal.find_element(By.ID, "showLargeModal").click()
+#     waiter5.until(EC.presence_of_element_located((By.XPATH, '//div[@role="dialog"]')))
+#     modal.switch_to.active_element.find_element(By.XPATH, '//div[@role="dialog"]')
+#     lm_title = modal.find_element(By.CLASS_NAME, "modal-title").text
+#     lm_body = modal.find_element(By.CLASS_NAME, "modal-body").text
+#     lm_button = modal.find_element(By.ID, "closeLargeModal")
+#     lm_button_text = lm_button.text
+#     lm_button.click()
+#
+#     assert lm_title == "Large Modal"
+#     assert lm_body == lm_text
+#     assert lm_button_text == "Close"
