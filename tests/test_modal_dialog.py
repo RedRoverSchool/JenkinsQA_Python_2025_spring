417b4ca8ec368cb63a1ee610568edcbf57c2a2dd
ppashchenkov <ppashchenkov@gmail.com>
2025-04-13 21:56:54 +0300

    Added test_modal_dialog.py

--- tests/test_modal_dialog.py
+++ tests/test_modal_dialog.py
@@ -1,5 +1,3 @@
-from time import sleep
-
 import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
@@ -33,19 +31,3 @@ def test_modal_dialog(modal, body_text, id_show, id_button, title):
     assert m_title == title
     assert m_body[0] == body_text
     assert m_button_text == "Close"
-
-# def test_large_modal_dialog(modal):
-#     lm_text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-#     waiter5 = WebDriverWait(modal, 5)
-#     modal.find_element(By.ID, "showLargeModal").click()
-#     waiter5.until(EC.presence_of_element_located((By.XPATH, '//div[@role="dialog"]')))
-#     modal.switch_to.active_element.find_element(By.XPATH, '//div[@role="dialog"]')
-#     lm_title = modal.find_element(By.CLASS_NAME, "modal-title").text
-#     lm_body = modal.find_element(By.CLASS_NAME, "modal-body").text
-#     lm_button = modal.find_element(By.ID, "closeLargeModal")
-#     lm_button_text = lm_button.text
-#     lm_button.click()
-#
-#     assert lm_title == "Large Modal"
-#     assert lm_body == lm_text
-#     assert lm_button_text == "Close"
