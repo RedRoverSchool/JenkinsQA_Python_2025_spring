28f5d6431a54d33224f455cfb094a94d8bf9578d
boxmykola <boxykola@gmail.com>
2025-04-13 02:18:05 -0600

    add test_accepted_user_names_are_shown_to_user

---
+++ tests/test_selenium.py
@@ -0,0 +1,21 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+@pytest.fixture
+def sauce(driver):
+    driver.get("https://www.saucedemo.com/")
+    return driver 
+    
+def test_accepted_user_names_are_shown_to_user(sauce):
+    user_names_node = sauce.find_element(By.XPATH,"//div[@id='login_credentials']")
+    user_names_text = user_names_node.text
+
+    for child in user_names_node.find_elements(By.XPATH,"./*"):
+        user_names_text = user_names_text.replace(child.text, "",1).strip()
+    
+    actual_user_names = user_names_text.splitlines()
+    expected_user_names = ["standard_user", "locked_out_user", "problem_user", "performance_glitch_user", "error_user", "visual_user"]
+
+    assert len(actual_user_names) == len(expected_user_names)
+    assert all([a == b for a, b in zip(actual_user_names, expected_user_names)])
