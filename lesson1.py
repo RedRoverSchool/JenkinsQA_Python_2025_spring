05e84b9852ada1a1958050db60a5dd58a7984b7e
DariaYasni <d.iasnikova@gmail.com>
2025-04-15 00:55:07 +0200

    test commit

---
+++ lesson1.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+# Настройки браузера
+options = Options()
+options.add_argument("--start-maximized")  # Открыть окно в полном размере
+# options.add_argument("--headless")      # Раскомментируй, если хочешь headless режим
+
+# Установка и запуск драйвера
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=options)
+
+# Тестовая загрузка страницы
+driver.get("https://www.google.com")
+print("Заголовок:", driver.title)
+
+driver.quit()
+
