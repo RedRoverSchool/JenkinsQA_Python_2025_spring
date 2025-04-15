0dd92347b5f8fd9db222ea5d3e4955b1485e7c12
lenka0376 <elena.goren@gmail.com>
2025-04-14 21:59:49 -0500

    add new test

---
+++ tests/test_lena_swag.py
@@ -0,0 +1,46 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+import pytest
+
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome()
+    driver.get("https://www.saucedemo.com/")
+    driver.find_element(By.ID, 'user-name').send_keys("standard_user")
+    driver.find_element(By.ID, 'password').send_keys("secret_sauce")
+    driver.find_element(By.NAME, 'login-button').click()
+    yield driver
+    driver.quit()
+
+def test_logo(driver):
+    assert "Swag Labs" in driver.title
+
+def test_add_to_cart(driver):
+    driver.find_element(By.NAME, 'add-to-cart-sauce-labs-backpack').click()
+    button_name = driver.find_element(By.NAME, 'remove-sauce-labs-backpack')
+    shopping_cart = driver.find_element(By.CLASS_NAME, 'shopping_cart_badge')
+    assert button_name.text == "Remove"
+    assert shopping_cart.text == "1"
+    driver.quit()
+
+def test_price_sort_low_to_high(driver):
+    select_element = Select(driver.find_element(By.CLASS_NAME, 'product_sort_container'))
+    select_element.select_by_value('lohi')
+    price_elements = driver.find_elements(By.CLASS_NAME, 'inventory_item_price')
+    prices = []
+    for price in price_elements:
+        price_text = price.text.replace('$', '')
+        prices.append(float(price_text))
+    sorted_prices = sorted(prices)
+    assert prices == sorted_prices, f"Prices are not sorted! Actual: {prices} Expected: {sorted_prices}"
+    driver.quit()
+
+def test_price_sort_high_to_low(driver):
+    select_element = Select(driver.find_element(By.CLASS_NAME, 'product_sort_container'))
+    select_element.select_by_value('hilo')
+    price_elements = driver.find_elements(By.CLASS_NAME, 'inventory_item_price')
+    prices = [float(price.text.replace('$', '')) for price in price_elements]
+    sorted_prices = sorted(prices, reverse=True)
+    assert prices == sorted_prices, f"Prices are not sorted correctly! Actual: {prices} Expected: {sorted_prices}"
+    driver.quit()
