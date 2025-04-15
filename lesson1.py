bbfd0e87a6b4bd7b5dea646f5a9b71725b87ead1
DariaYasni <d.iasnikova@gmail.com>
2025-04-15 09:36:03 +0200

    test commit

--- lesson1.py
+++ lesson1.py
@@ -13,7 +13,7 @@ service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=options)
 
 # Тестовая загрузка страницы
-driver.get("https://www.google.com"++++)
+driver.get("https://www.yahoo.com")
 print("Заголовок:", driver.title)
 
 driver.quit()
