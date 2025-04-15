ee903e031e82acd1b46053831ef9e254481eec95
pav1222 <pav333zt@gmail.com>
2025-04-15 17:28:56 +0300

    2

--- tests/test_login_Jenkins.py
+++ tests/test_login_Jenkins.py
@@ -1,13 +1,24 @@
+import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-def test_login():
+@pytest.fixture
+def authorization_jenkins():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--lang=en")
+
     driver = webdriver.Chrome()
     driver.get("http://localhost:8080/login?from=%2F")
     driver.find_element(By.NAME, "j_username").send_keys("admin")
     driver.find_element(By.NAME, "j_password").send_keys("admin")
-    driver.find_element(By.XPATH, "//*[@id='main-panel']/div/form/button").click()
+    return driver
+
+
+
+def test_login(authorization_jenkins):
+    driver = authorization_jenkins
+    driver.find_element(By.XPATH, "//button[@name='Submit']").click()
 
     assert driver.find_element(By.ID,"jenkins-name-icon")
     driver.quit()
