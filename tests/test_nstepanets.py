fa122e6566623d649e7b28552ecd91d3e34f7626
Mykola Stepanets <nik.stepanets@gmail.com>
2025-04-17 23:36:47 +0300

    Use fixtures from conftest.py

--- tests/test_nstepanets.py
+++ tests/test_nstepanets.py
@@ -1,16 +1,7 @@
 import pytest
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
-
-@pytest.fixture(scope="session")
-def driver():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-    yield driver
-    driver.quit()
 
 
 @pytest.fixture(scope="function")
@@ -18,25 +9,21 @@ def login_page(driver):
     driver.get("https://practicetestautomation.com/practice-test-login/")
     return driver
 
-
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 def wait(driver):
     driver_wait = WebDriverWait(driver, 1)
     return driver_wait
-
 
 
 def test_login_page_opens(login_page):
     header = login_page.find_element(By.XPATH, '//*[@id="login"]/h2')
     assert header.text == "Test login"
 
-
 def test_login_successful(login_page):
     login_page.find_element(By.ID, "username").send_keys("student")
     login_page.find_element(By.ID, "password").send_keys("Password123")
     login_page.find_element(By.XPATH, '//button[@id="submit"]').click()
     assert "practicetestautomation.com/logged-in-successfully/" in login_page.current_url
-
 
 def test_login_with_invalid_password(login_page, wait):
     login_page.find_element(By.ID, "username").send_keys("student")
@@ -45,7 +32,6 @@ def test_login_with_invalid_password(login_page, wait):
     error_message = login_page.find_element(By.XPATH, '//div[@id="error"]')
     wait.until(EC.visibility_of(error_message), "Error message not displayed")
     assert error_message.text == "Your password is invalid!"
-
 
 def test_login_with_invalid_username(login_page, wait):
     login_page.find_element(By.ID, "username").send_keys("incorrectUser")
