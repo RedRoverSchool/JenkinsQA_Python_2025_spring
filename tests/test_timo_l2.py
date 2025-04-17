a4bb505401ef8803ae3eff4d4656f04c964e0e71
AlexandrTimo <mr.atimoshenko@gmail.com>
2025-04-16 20:17:00 -0700

    added changes

--- tests/test_timo_l2.py
+++ tests/test_timo_l2.py
@@ -4,7 +4,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import ElementClickInterceptedException
-
 '''
 https://www.toyota.com/4runner/
 '''
