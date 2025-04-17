420c35c4545096cc0b58ef4f1a64cec73e8a6d84
Alesia Kuptsova <alesya.kuptsova@gmail.com>
2025-04-17 22:23:49 +0900

    improve wait in test

--- tests/test_kuptsova_alesya_second_task.py
+++ tests/test_kuptsova_alesya_second_task.py
@@ -1,4 +1,6 @@
 import pytest
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
 
 username_correct = 'problem_user'
@@ -8,7 +10,6 @@ password_correct = 'secret_sauce'
 @pytest.fixture
 def setup_driver(driver):
     driver.get("https://www.saucedemo.com/")
-    driver.implicitly_wait(5)
     return driver
 
 
@@ -20,10 +21,13 @@ def test_log_in_with_unfilled_fields(setup_driver):
 
 
 def test_user_can_log_out_of_the_site(setup_driver):
+    wait5 = WebDriverWait(setup_driver, 5)
     setup_driver.find_element(By.ID, 'user-name').send_keys(username_correct)
     setup_driver.find_element(By.ID, 'password').send_keys(password_correct)
     setup_driver.find_element(By.NAME, 'login-button').click()
+    wait5.until(expected_conditions.visibility_of_element_located((By.ID, 'react-burger-menu-btn')))
     setup_driver.find_element(By.ID, 'react-burger-menu-btn').click()
+    wait5.until(expected_conditions.visibility_of_element_located((By.ID, 'logout_sidebar_link')))
     setup_driver.find_element(By.ID, 'logout_sidebar_link').click()
 
     assert setup_driver.current_url == 'https://www.saucedemo.com/', 'wrong url'
