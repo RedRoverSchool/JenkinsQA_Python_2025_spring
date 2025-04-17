34de0d5e009f722ad6a99d3bf3e6dc57ae27b49b
DariaYasni <d.iasnikova@gmail.com>
2025-04-17 09:24:39 +0200

    trial test_googlesearch.py

--- old/test_googlesearch.py
+++ old/test_googlesearch.py
@@ -4,4 +4,4 @@ from webdriver_manager.chrome import ChromeDriverManager
 def test_open_google():
     driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.get("https://www.google.com")
-    driver.quit()+++
+    driver.quit()
