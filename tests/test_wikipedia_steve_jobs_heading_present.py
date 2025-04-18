d952a26050192c6146ff48571146684b649e8086
Polina <thepolinaguller@gmail.com>
2025-04-18 14:06:57 +0300

    Use project driver fixture and explicit waits in Wikipedia test

--- tests/test_wikipedia_steve_jobs_heading_present.py
+++ tests/test_wikipedia_steve_jobs_heading_present.py
@@ -1,21 +1,19 @@
 from time import sleep
-
 import pytest
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
-@pytest.fixture
-def driver():
-    # driver settings
-    options = webdriver.ChromeOptions()
-    driver = webdriver.Chrome(options=options)
-    driver.set_window_size(1920, 1080)
-    driver.implicitly_wait(5)  # we wait up to 5 seconds to find the elements
-    yield driver
-    driver.quit()  # Closing the browser after completing the test
+#
+# @pytest.fixture
+# def driver():
+#     # driver settings
+#     options = webdriver.ChromeOptions()
+#     driver = webdriver.Chrome(options=options)
+#     driver.set_window_size(1920, 1080)
+#     driver.implicitly_wait(5)  # we wait up to 5 seconds to find the elements
+#     yield driver
+#     driver.quit()  # Closing the browser after completing the test
 
 
 def test_wikipedia_steve_jobs_heading_present(driver):
