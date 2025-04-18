3966320fa587df63ad9ca18805a1ac1ebaf99d08
Alesia Kuptsova <alesya.kuptsova@gmail.com>
2025-04-18 22:19:12 +0900

    try to fix timeouts

--- tests/test_kuptsova_alesya_second_task.py
+++ tests/test_kuptsova_alesya_second_task.py
@@ -8,27 +8,27 @@ password_correct = 'secret_sauce'
 
 
 @pytest.fixture
-def setup_driver(driver):
+def saucedemo(driver):
     driver.get("https://www.saucedemo.com/")
     return driver
 
 
-def test_log_in_with_unfilled_fields(setup_driver):
-    setup_driver.find_element(By.NAME, 'login-button').click()
+def test_log_in_with_unfilled_fields(saucedemo):
+    saucedemo.find_element(By.NAME, 'login-button').click()
 
-    error_message = setup_driver.find_element(By.XPATH, "//h3[@data-test='error']")
+    error_message = saucedemo.find_element(By.XPATH, "//h3[@data-test='error']")
     assert error_message.text == 'Epic sadface: Username is required'
 
 
-def test_user_can_log_out_of_the_site(setup_driver):
-    wait = WebDriverWait(setup_driver, 50)
-    setup_driver.find_element(By.ID, 'user-name').send_keys(username_correct)
-    setup_driver.find_element(By.ID, 'password').send_keys(password_correct)
-    setup_driver.find_element(By.NAME, 'login-button').click()
-    wait.until(EC.visibility_of_element_located((By.ID, 'react-burger-menu-btn')))
-    setup_driver.find_element(By.ID, 'react-burger-menu-btn').click()
-    wait.until(EC.visibility_of_element_located((By.ID, 'logout_sidebar_link')))
-    setup_driver.find_element(By.ID, 'logout_sidebar_link').click()
+def test_user_can_log_out_of_the_site(saucedemo):
+    wait = WebDriverWait(saucedemo, 5)
+    saucedemo.find_element(By.ID, 'user-name').send_keys(username_correct)
+    saucedemo.find_element(By.ID, 'password').send_keys(password_correct)
+    saucedemo.find_element(By.NAME, 'login-button').click()
+    wait.until(EC.element_to_be_clickable((By.ID, 'react-burger-menu-btn')))
+    saucedemo.find_element(By.ID, 'react-burger-menu-btn').click()
+    wait.until(EC.element_to_be_clickable((By.ID, 'logout_sidebar_link')))
+    saucedemo.find_element(By.ID, 'logout_sidebar_link').click()
     wait.until(EC.visibility_of_element_located((By.NAME, 'login-button')))
 
-    assert setup_driver.current_url == 'https://www.saucedemo.com/', 'wrong url'
+    assert saucedemo.current_url == 'https://www.saucedemo.com/', 'wrong url'
