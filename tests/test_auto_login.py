aef22183baa74971c9171733c85df5e796858ee9
YanaLifanova <janali.sqa@gmail.com>
2025-04-19 09:49:41 -0500

    making changes in assert home page

--- tests/test_auto_login.py
+++ tests/test_auto_login.py
@@ -1,23 +1,24 @@
 from _ast import Assert
 
 import pytest
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.remote.webelement import WebElement
-from faker import Faker
+
 
 
 @pytest.fixture
 def auto_login(driver):
     driver.get("http://automationexercise.com")
-    return driver
-
+    yield driver
+    driver.quit()
 
 def test_register_user(auto_login):
 
 
     auto_login.get("https://www.automationexercise.com/")
-    newUserSignup = auto_login.find_element(By.CSS_SELECTOR,  '.nav a[href="/"]')
-    assert newUserSignup.is_displayed() , "User signup display is not visible"
+    home = auto_login.find_element(By.CSS_SELECTOR,  '.nav a[href="/"]')
+    assert home.is_displayed()
+    current_url = auto_login.current_url
+    expected_url ="https://www.automationexercise.com/"
+    assert current_url == expected_url, f"Expected URL: {expected_url}, but got: {current_url}"
+    print("The page is correct. Current URL:", current_url)
 
-    auto_login.quit()
