e296e85c97190ea29695cf23f995fbd910e3cf39
ppashchenkov <email@example.com>
2025-04-23 15:40:48 +0300

    TC_02.003.01 | Freestyle Project Configuration> Configure SCM

--- tests/freestyle_project/test_scm_freestyle_project.py
+++ tests/freestyle_project/test_scm_freestyle_project.py
@@ -8,10 +8,6 @@ from selenium.webdriver import ActionChains
 from tests.freestyle_project.freestyle_data import Freestyle
 
 
-def test_scm_to_none(freestyle):
-    wait = WebDriverWait(freestyle, 10)
-    freestyle.find_element(By.XPATH, '//button[@name="Submit"]').click()
-    wait.until(EC.presence_of_element_located((By.LINK_TEXT, 'Build Now'))).click()
-    h1_text = freestyle.find_element(By.CSS_SELECTOR, 'h1').text
+def test_scm_to_none(empty_configure):
 
-    assert h1_text == Freestyle.project_name
+    assert empty_configure == True
