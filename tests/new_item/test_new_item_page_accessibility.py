cf046ca44b6ab15ab1180014942c13d3a4e0ed2e
Maksim Chuvashov <pardon1015@gmail.com>
2025-04-22 21:43:07 -0700

    Fix test assert

--- tests/new_item/test_new_item_page_accessibility.py
+++ tests/new_item/test_new_item_page_accessibility.py
@@ -7,6 +7,7 @@ def test_new_item_page_is_accessible(main_page):
     wait = WebDriverWait(main_page, 5)
 
     main_page.find_element(By.LINK_TEXT, "New Item").click()
-    wait.until(EC.url_to_be("http://localhost:8080/view/all/newJob"))
-    title = main_page.title
-    assert "new item" in title.lower()
+    wait.until(EC.url_contains("newJob"))
+
+    assert main_page.current_url.endswith("newJob")
+    assert "new item" in main_page.title.lower()
