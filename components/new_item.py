d96b60882c9854a8c79e85d50a802ffb10bfd611
Zarina <zaya110589@gmail.com>
2025-04-23 00:01:24 -0400

    didnt pass ci. returned previous version

--- components/new_item.py
+++ components/new_item.py
@@ -1,22 +1,24 @@
+import time
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 class NewItem:
     def __init__(self, main_page):
         self.main_page = main_page
 
     def create_freestyle_project(self):
-        wait = WebDriverWait(self.main_page, 5)
-        wait.until(EC.element_to_be_clickable((By.XPATH, "//a[@href='/view/all/newJob']"))).click()
-        wait.until(EC.element_to_be_clickable((By.ID, "name"))).send_keys("freestyle1")
-        wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "hudson_model_FreeStyleProject"))).click()
-        wait.until(EC.element_to_be_clickable((By.ID, "ok-button"))).click()
-        wait.until(EC.element_to_be_clickable((By.NAME, "Submit"))).click()
-        wait.until(EC.element_to_be_clickable((By.ID, "jenkins-home-link"))).click()
+        self.main_page.find_element(By.XPATH, "//a[@href ='/view/all/newJob']").click()
+        time.sleep(2)
+        self.main_page.find_element(By.ID, "name").send_keys("freestyle1")
+        self.main_page.find_element(By.CLASS_NAME, "hudson_model_FreeStyleProject").click()
+        self.main_page.find_element(By.ID, "ok-button").click()
+        time.sleep(2)
+        self.main_page.find_element(By.NAME, "Submit").click()
+        time.sleep(2)
+        self.main_page.find_element(By.ID, "jenkins-home-link").click()
+
     def copy_from_option_exist(self):
-        wait = WebDriverWait(self.main_page, 5)
-        wait.until(EC.element_to_be_clickable((By.XPATH, "//a[@href ='/view/all/newJob']"))).click()
-        copyFromBtn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input.jenkins-input.auto-complete")))
+        self.main_page.find_element(By.XPATH, "//a[@href ='/view/all/newJob']").click()
+        time.sleep(2)
+        copyFromBtn = self.main_page.find_element(By.CSS_SELECTOR, "input.jenkins-input.auto-complete")
         assert copyFromBtn.is_displayed()
 
