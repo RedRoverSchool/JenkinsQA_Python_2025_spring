b786d23820572c81d8fca49a1d31b090b5df049a
KatyaMy <20FamGam20@gmail.com>
2025-04-24 15:21:24 -0400

    wait + 5 sec TC_01.004.01 | New Item > Folder > Create a New Folder

--- tests/new_item/test_create_new_item.py
+++ tests/new_item/test_create_new_item.py
@@ -1,11 +1,11 @@
+import time
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait as wait
 
-from conftest import main_page
-
 new_folder_name = 'Test_Folder'
-DEFAULT_TIMEOUT = 10
+DEFAULT_TIMEOUT = 15
 
 
 def wait_for(driver, by, selector, timeout=DEFAULT_TIMEOUT):
@@ -14,7 +14,7 @@ def wait_for(driver, by, selector, timeout=DEFAULT_TIMEOUT):
     )
 
 
-def test_check_create_new_item(new_item_page,jenkins_reset):
+def test_check_create_new_item(new_item_page, jenkins_reset):
     wait_for(new_item_page, By.CSS_SELECTOR, '#name').send_keys(new_folder_name)
     folder_option = wait_for(new_item_page, By.CSS_SELECTOR, '[class*="cloudbees_hudson_plugins_folder"]')
     folder_option.click()
@@ -25,5 +25,5 @@ def test_check_create_new_item(new_item_page,jenkins_reset):
     wait_for(new_item_page, By.CSS_SELECTOR, "#jenkins-name-icon").click()
 
     assert new_item_page.find_elements(By.XPATH,
-                                   f"//table[@id='projectstatus']//tbody//tr[contains(., '{new_folder_name}')]"), \
+                                       f"//table[@id='projectstatus']//tbody//tr[contains(., '{new_folder_name}')]"), \
         f"Folder '{new_folder_name}' NOT FOUND"
