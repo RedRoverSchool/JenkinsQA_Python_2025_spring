c83114a62b69797eb2d5a7907c15ab7acda02e51
ppashchenkov <email@example.com>
2025-04-24 13:42:18 +0300

    TC_02.003.01 | Freestyle Project Configuration> Configure SCM

--- tests/freestyle_project/test_scm_freestyle_project.py
+++ tests/freestyle_project/test_scm_freestyle_project.py
@@ -32,15 +32,15 @@ def test_tooltips(freestyle, tp_link, tp_wait, tp_expected_text, count):
     actions.scroll_by_amount(0, to_half).perform()
     git.click()
     freestyle.find_element(By.XPATH, '//button[@name="Apply"]').click()
-    actions.pause(2).scroll_by_amount(0, to_dec * count).perform()
-    if 3 < count < 5:
+    actions.pause(1).scroll_by_amount(0, (to_dec * count)//1).perform()
+    if count < 5:
         advanced = freestyle.find_element(By.XPATH, '//div[@class="form-container tr"]//div[@class="jenkins-form-item tr"]//button')
         advanced.click()
         freestyle.find_element(By.XPATH, '//button[@name="Apply"]').click()
     else:
         wait.until(EC.visibility_of_element_located((By.XPATH, tp_link)))
     tooltip_link = freestyle.find_element(By.XPATH, tp_link)
-    actions.move_to_element(tooltip_link).perform()
+    actions.pause(1).move_to_element(tooltip_link).perform()
     wait.until(EC.presence_of_element_located((By.XPATH, tp_wait)))
     tp_text = freestyle.find_element(By.XPATH, '//div[@class="tippy-content"]').text
 
