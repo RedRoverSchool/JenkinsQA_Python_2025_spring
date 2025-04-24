e39159f5a4b72ac4b91d695c761609628d57de82
Zarina <zaya110589@gmail.com>
2025-04-23 23:28:46 -0400

    refactored and added new test

--- components/new_item.py
+++ components/new_item.py
@@ -5,13 +5,13 @@ from selenium.webdriver.support import expected_conditions as EC
 class NewItem:
     def __init__(self, main_page):
         self.main_page = main_page
-        self.wait = WebDriverWait(self.main_page, 10)
+        self.wait = WebDriverWait(self.main_page, 20)
 
     def click_element(self, by, value):
         self.wait.until(EC.element_to_be_clickable((by, value))).click()
 
     def enter_text(self, by, value, text):
-        self.wait.until(EC.presence_of_element_located((by, value))).send_keys(text)
+        self.wait.until(EC.element_to_be_clickable((by, value))).send_keys(text)
 
     def create_freestyle_project(self):
         self.click_element(By.XPATH, "//a[@href='/view/all/newJob']")
@@ -20,17 +20,17 @@ class NewItem:
         self.click_element(By.ID, "ok-button")
         self.click_element(By.NAME, "Submit")
         self.click_element(By.ID, "jenkins-home-link")
+
     def copy_from_option_exist(self):
-        wait = WebDriverWait(self.main_page, 10)
-        wait.until(EC.element_to_be_clickable((By.XPATH, "//a[@href='/view/all/newJob']"))).click()
-        copyFromBtn = wait.until(
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, "//a[@href='/view/all/newJob']"))).click()
+        copyFromBtn = self.wait.until(
             EC.visibility_of_element_located((By.CSS_SELECTOR, "input.jenkins-input.auto-complete")))
         assert copyFromBtn.is_displayed(), "Autocomplete input field is not visible!"
+
     def copy_from_option_is_working(self):
-        wait = WebDriverWait(self.main_page, 10)
-        wait.until(EC.element_to_be_clickable((By.XPATH, "//a[@href='/view/all/newJob']"))).click()
-        copyFromBtn = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "input.jenkins-input.auto-complete")))
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, "//a[@href='/view/all/newJob']"))).click()
+        copyFromBtn = self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "input.jenkins-input.auto-complete")))
         copyFromBtn.send_keys("f")
-        wait.until(
+        self.wait.until(
             EC.text_to_be_present_in_element_attribute((By.CSS_SELECTOR, "input.jenkins-input.auto-complete"), "aria-expanded", "true"))
         assert copyFromBtn.is_displayed(), "Autocomplete input field is not visible!"
