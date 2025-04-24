60d2bdd0680a77a3a713c8d0efdfd07b28eb3cdc
KatyaMy <20FamGam20@gmail.com>
2025-04-24 17:21:45 -0400

    TC_01.004.01 | New Item > Folder > Create a New Folder

--- tests/new_item/test_create_new_item.py
+++ tests/new_item/test_create_new_item.py
@@ -20,15 +20,16 @@ def wait_for_clickable(driver, by, selector, timeout=DEFAULT_TIMEOUT):
     )
 
 
-def test_check_create_new_item(main_page):
+def test_check_create_new_item(main_page, config):
     new_item_button = wait_for(main_page, By.CSS_SELECTOR, "a[href='/view/all/newJob']")
     new_item_button.click()
     wait_for(main_page, By.CSS_SELECTOR, '#name').send_keys(new_folder_name)
     wait_for_clickable(main_page, By.CSS_SELECTOR, '[class*="cloudbees_hudson_plugins_folder"]').click()
     wait_for_clickable(main_page, By.CSS_SELECTOR, '#ok-button').click()
     wait_for_clickable(main_page, By.CSS_SELECTOR, '[name=Submit]').click()
-    jenkins_icon = wait_for_clickable(main_page, By.CSS_SELECTOR, "#jenkins-home-link")
-    jenkins_icon.click()
-    time.sleep(4)
+    # jenkins_icon = wait_for_clickable(main_page, By.CSS_SELECTOR, "#jenkins-home-link")
+    # jenkins_icon.click()
+    main_page.get(config.jenkins.base_url)
+
     assert wait_for(main_page, By.XPATH,f"//table[@id='projectstatus']/tbody/tr[contains(., '{new_folder_name}')]"), \
         f"Folder '{new_folder_name}' NOT FOUND"
