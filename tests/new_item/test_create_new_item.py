90b9a37f51b22e1fcb019f9b02f1bd167237b9ab
KatyaMy <20FamGam20@gmail.com>
2025-04-24 15:35:43 -0400

    added wait btn click; TC_01.004.01 | New Item > Folder > Create a New Folder

--- tests/new_item/test_create_new_item.py
+++ tests/new_item/test_create_new_item.py
@@ -3,7 +3,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait as wait
 
 new_folder_name = 'Test_Folder'
-DEFAULT_TIMEOUT = 15
+DEFAULT_TIMEOUT = 10
 
 
 def wait_for(driver, by, selector, timeout=DEFAULT_TIMEOUT):
@@ -12,7 +12,7 @@ def wait_for(driver, by, selector, timeout=DEFAULT_TIMEOUT):
     )
 
 
-def test_check_create_new_item(new_item_page, jenkins_reset):
+def test_check_create_new_item(new_item_page):
     wait_for(new_item_page, By.CSS_SELECTOR, '#name').send_keys(new_folder_name)
     wait_for(new_item_page, By.CSS_SELECTOR, '[class*="cloudbees_hudson_plugins_folder"]').click()
     wait_for(new_item_page, By.CSS_SELECTOR, '#ok-button').click()
