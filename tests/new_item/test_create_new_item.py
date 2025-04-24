fd40d86b672eff60a09c1a32ca4e2438e92ddea0
KatyaMy <20FamGam20@gmail.com>
2025-04-24 16:53:42 -0400

    new selector for jenkins link; TC_01.004.01 | New Item > Folder > Create a New Folder

--- tests/new_item/test_create_new_item.py
+++ tests/new_item/test_create_new_item.py
@@ -25,8 +25,8 @@ def test_check_create_new_item(main_page):
     wait_for_clickable(main_page, By.CSS_SELECTOR, '[class*="cloudbees_hudson_plugins_folder"]').click()
     wait_for_clickable(main_page, By.CSS_SELECTOR, '#ok-button').click()
     wait_for_clickable(main_page, By.CSS_SELECTOR, '[name=Submit]').click()
-    wait_for_clickable(main_page, By.XPATH, "//li[@class='jenkins-breadcrumbs__list-item']/a[contains(text(), 'Dashboard')]").click()
+    jenkins_icon = wait_for_clickable(main_page, By.CSS_SELECTOR, "#jenkins-home-link")
+    jenkins_icon.click()
 
-    assert main_page.find_elements(By.XPATH,
-                                   f"//table[@id='projectstatus']//tbody//tr[contains(., '{new_folder_name}')]"), \
+    assert wait_for(main_page, By.XPATH,f"//table[@id='projectstatus']/tbody/tr[contains(., '{new_folder_name}')]"), \
         f"Folder '{new_folder_name}' NOT FOUND"
