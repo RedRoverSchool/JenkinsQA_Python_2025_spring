7f86d6eb554bad21132e91ef06119091033bfdd4
KatyaMy <20FamGam20@gmail.com>
2025-04-24 17:04:25 -0400

    added wait+30sec;TC_01.004.01 | New Item > Folder > Create a New Folder

--- tests/new_item/test_create_new_item.py
+++ tests/new_item/test_create_new_item.py
@@ -3,7 +3,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait as wait
 
 new_folder_name = 'Test_Folder'
-DEFAULT_TIMEOUT = 25
+DEFAULT_TIMEOUT = 50
 
 
 def wait_for(driver, by, selector, timeout=DEFAULT_TIMEOUT):
@@ -28,5 +28,5 @@ def test_check_create_new_item(main_page):
     jenkins_icon = wait_for_clickable(main_page, By.CSS_SELECTOR, "#jenkins-home-link")
     jenkins_icon.click()
 
-    # assert wait_for(main_page, By.XPATH,f"//table[@id='projectstatus']/tbody/tr[contains(., '{new_folder_name}')]"), \
-    #     f"Folder '{new_folder_name}' NOT FOUND"
+    assert wait_for(main_page, By.XPATH,f"//table[@id='projectstatus']/tbody/tr[contains(., '{new_folder_name}')]"), \
+        f"Folder '{new_folder_name}' NOT FOUND"
