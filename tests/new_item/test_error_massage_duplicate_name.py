b2e1f74ae1a5817d6d3aa96009c0cf02bff49e65
lenka0376 <elena.goren@gmail.com>
2025-04-25 14:35:26 -0500

    update wait

--- tests/new_item/test_error_massage_duplicate_name.py
+++ tests/new_item/test_error_massage_duplicate_name.py
@@ -8,19 +8,15 @@ import time
 def test_check_error_message(new_item_page):
     wait = WebDriverWait(new_item_page, timeout=5)
     testjob = "job" + str(random.randint(1,100))
-    name_field = wait.until(
-        EC.visibility_of_element_located((By.CSS_SELECTOR, "#name")))
-    name_field.send_keys(testjob)
+    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#name"))).send_keys(testjob)
     new_item_page.find_element(By.CLASS_NAME, "hudson_model_FreeStyleProject").click()
     new_item_page.find_element(By.CSS_SELECTOR, "#ok-button").click()
-    dashboard = WebDriverWait(new_item_page, 5).until(
-        EC.visibility_of_element_located((By.CSS_SELECTOR, '#breadcrumbBar  a[href="/"]')))
-    dashboard.click()
+    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '#breadcrumbBar  a[href="/"]'))).click()
     new_item_page.find_element(By.CSS_SELECTOR, "a[href='/view/all/newJob']").click()
-    name_field = wait.until(
-        EC.visibility_of_element_located((By.CSS_SELECTOR, "#name")))
-    name_field.send_keys(testjob)
-    assert new_item_page.find_element(By.CSS_SELECTOR, "#itemname-invalid").text == f"» A job already exists with the name ‘{testjob}’"
+    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#name"))).send_keys(testjob)
+    target_text = new_item_page.find_element(By.CSS_SELECTOR, "#itemname-invalid").text
+    assert target_text == f"» A job already exists with the name ‘{testjob}’"
+
 
 
 
