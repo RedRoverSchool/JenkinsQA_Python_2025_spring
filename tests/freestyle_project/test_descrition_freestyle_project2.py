761eafa2f59e94d21e93071f47a383f9e2a7ab30
pav1222 <pav333zt@gmail.com>
2025-04-25 18:49:41 +0300

    2

--- tests/freestyle_project/test_descrition_freestyle_project2.py
+++ tests/freestyle_project/test_descrition_freestyle_project2.py
@@ -4,16 +4,9 @@ from selenium.webdriver.support import expected_conditions as EC
 
 from tests.freestyle_project.freestyle_data import Freestyle
 
-text = Freestyle.description_text
 
-def test_freestyle(freestyle,main_page):
-    wait5 = WebDriverWait(main_page, 5)
-    main_page.find_element(By.NAME, "description").send_keys(text)
-    wait5.until(EC.visibility_of_element_located((By.NAME, "Submit"))).click()
-
-def test_text_in_description(description_page):
-    container = description_page.find_element(By.CSS_SELECTOR, "#description")
-    assert text in container
+def test_text_in_description(description_appears):
+    assert Freestyle.description_text in description_appears
 
 
 
