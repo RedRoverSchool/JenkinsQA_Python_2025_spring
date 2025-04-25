340c87f4dcfc19e1d9cec79b8bb82732aecf86cd
pav1222 <pav333zt@gmail.com>
2025-04-25 18:52:51 +0300

    3

--- tests/freestyle_project/test_descrition_freestyle_project2.py
+++ tests/freestyle_project/test_descrition_freestyle_project2.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from tests.freestyle_project.freestyle_data import Freestyle
 
 
+
 def test_text_in_description(description_appears):
     assert Freestyle.description_text in description_appears
 
