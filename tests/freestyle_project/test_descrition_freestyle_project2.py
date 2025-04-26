cc9021b6108b1374e0c9be9f8e04344076e71df3
pav1222 <pav333zt@gmail.com>
2025-04-26 07:53:18 +0300

    CheckFreestyleDescriptionТС_02.002.03

--- tests/freestyle_project/test_descrition_freestyle_project2.py
+++ tests/freestyle_project/test_descrition_freestyle_project2.py
@@ -1,13 +1,4 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
 from tests.freestyle_project.freestyle_data import Freestyle
-
-
 
 def test_text_in_description(description_appears):
     assert Freestyle.description_text in description_appears
-
-
-
