8489dfb47ee2403c464527c40acfc36f301bb4cd
Maksim Chuvashov <pardon1015@gmail.com>
2025-04-26 21:48:25 -0700

    Remove one assert and change wait

--- tests/new_item/test_new_item_page_availability.py
+++ tests/new_item/test_new_item_page_availability.py
@@ -3,11 +3,10 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-def test_new_item_page_is_accessible(main_page):
+def test_new_item_page_is_available(main_page):
     wait = WebDriverWait(main_page, 5)
 
     main_page.find_element(By.LINK_TEXT, "New Item").click()
-    wait.until(EC.url_contains("newJob"))
+    wait.until(EC.visibility_of_element_located((By.XPATH, "//li[contains(text(),'New Item')]")))
 
-    assert main_page.current_url.endswith("newJob")
     assert "new item" in main_page.title.lower()
