5cb153f1a0a336e7fe7bfabbd1312ed4475a7964
Petrichor <180253417+IrinaPetrichor@users.noreply.github.com>
2025-04-30 17:18:03 -0400

    Check crumb

--- tests/freestyle_project/test_build_triggers.py
+++ tests/freestyle_project/test_build_triggers.py
@@ -1,5 +1,4 @@
 import logging
-from time import sleep
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
@@ -34,6 +33,11 @@ def test_user_can_trigger_builds_remotely(auth_token, freestyle, config):
 
     window_before = freestyle.window_handles[0]
     freestyle.switch_to.new_window()
+
+    crumb_issuer = "http://localhost:8080/crumbIssuer/api/json"
+    freestyle.get(crumb_issuer)
+    crumb = freestyle.find_element(By.CSS_SELECTOR, "body").text.split('"')[7]
+    logger.error(f"Crumb: {crumb}")
     freestyle.get(trigger_job_api)
     logger.error(f"Send request: {freestyle.current_url}")
     freestyle.switch_to.window(window_before)
@@ -42,19 +46,17 @@ def test_user_can_trigger_builds_remotely(auth_token, freestyle, config):
     wait10.until(EC.visibility_of_element_located((By.LINK_TEXT, "Dashboard"))).click()
     logger.error(f"On Dashboard: {freestyle.current_url}")
 
-    # build_job_link = wait10.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#buildQueue a[href*='/job']")))
-    #
-    # wait10.until(EC.text_to_be_present_in_element(
-    #     (By.CSS_SELECTOR, "#buildQueue a[href*='/job']"),
-    #     Freestyle.project_name
-    # ))
-    #
-    # wait60.until(EC.text_to_be_present_in_element(
-    #     (By.CSS_SELECTOR, "#buildQueue>.pane-content tbody>tr>td"),
-    #     "No builds in the queue.")
-    # )
+    build_job_link = wait10.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#buildQueue a[href*='/job']")))
 
-    sleep(120)
+    wait10.until(EC.text_to_be_present_in_element(
+        (By.CSS_SELECTOR, "#buildQueue a[href*='/job']"),
+        Freestyle.project_name
+    ))
+
+    wait60.until(EC.text_to_be_present_in_element(
+        (By.CSS_SELECTOR, "#buildQueue>.pane-content tbody>tr>td"),
+        "No builds in the queue.")
+    )
 
     logger.error(f"After build finished")
     freestyle.find_element(By.LINK_TEXT, "Build History").click()
