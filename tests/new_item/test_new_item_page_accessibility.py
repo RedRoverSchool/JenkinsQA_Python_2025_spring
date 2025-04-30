28815beeb0798d37a265bb4aa41e23bad3ddf103
OksanaVK <oksanaveligor@outlook.com>
2025-04-29 20:19:05 -0700

    Adding test for US_01.001: test_new_item_page_accessibility

---
+++ tests/new_item/test_new_item_page_accessibility.py
@@ -0,0 +1,11 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+def test_new_item_page_accessibility(main_page):
+    wait = WebDriverWait(main_page, 10)
+    expected_input_field_name = "Enter an item name"
+    wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="tasks"]/div[1]/span/a/span[1]'))).click()
+    field_name = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="createItem"]/div[1]/div/label')))
+    assert expected_input_field_name == field_name.text, f"field name is {field_name}, expected {expected_input_field_name}"
