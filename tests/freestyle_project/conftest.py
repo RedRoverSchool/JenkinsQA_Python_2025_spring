a9cb2cbef4298f5791b258f972f2e407195c10f2
Petrichor <180253417+IrinaPetrichor@users.noreply.github.com>
2025-05-01 06:02:03 -0400

    Clean code

--- tests/freestyle_project/conftest.py
+++ tests/freestyle_project/conftest.py
@@ -1,12 +1,8 @@
-import logging
-
 import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import ActionChains
-
-from conftest import main_page
 from tests.freestyle_project.freestyle_data import Freestyle
 
 
@@ -21,7 +17,6 @@ def freestyle(main_page, config):
 
     return main_page
 
-
 @pytest.fixture(scope="function")
 def tooltip(freestyle):
     wait = WebDriverWait(freestyle, 10)
@@ -32,7 +27,6 @@ def tooltip(freestyle):
 
     return freestyle.find_element(By.XPATH, '//div[@class="tippy-content"]').text
 
-
 @pytest.fixture(scope="function")
 def disabled_message(freestyle):
     wait = WebDriverWait(freestyle, 10)
@@ -42,7 +36,6 @@ def disabled_message(freestyle):
     wait.until(EC.presence_of_element_located((By.XPATH, '//form[@action="enable"]')))
 
     return freestyle
-
 
 @pytest.fixture(scope="function")
 def enable_automatically(disabled_message):
@@ -65,7 +58,6 @@ def enable_automatically(disabled_message):
 
     return [is_warning_message_disappear, is_project_enable]
 
-
 @pytest.fixture()
 def can_add_description(freestyle):
     wait = WebDriverWait(freestyle, 10)
@@ -74,7 +66,6 @@ def can_add_description(freestyle):
     freestyle.find_element(By.XPATH, '//button[@name="Apply"]').click()
 
     return freestyle.find_element(By.XPATH, '//textarea[@name="description"]').get_attribute("value")
-
 
 @pytest.fixture()
 def empty_configure(freestyle):
@@ -87,7 +78,6 @@ def empty_configure(freestyle):
     else:
         return Freestyle
 
-
 @pytest.fixture()
 def description_appears(freestyle):
     wait = WebDriverWait(freestyle, 10)
@@ -96,7 +86,6 @@ def description_appears(freestyle):
     wait.until(EC.presence_of_element_located((By.ID, 'description')))
 
     return freestyle.find_element(By.ID, 'description').text
-
 
 @pytest.fixture()
 def preview_hide(freestyle):
@@ -115,44 +104,51 @@ def preview_hide(freestyle):
 
     return [preview, hide]
 
-
 @pytest.fixture(scope="function")
-def revoke_tokens(main_page, config):
+def revoke_project_tokens(main_page, config):
+    """
+    Fixture to revoke project-specific tokens from the user's security settings
+    if they match the current project name defined in Freestyle.project_name.
+    """
     wait = WebDriverWait(main_page, 10)
 
     main_page.find_element(By.CSS_SELECTOR, 'a[href*="/user/"]').click()
     wait.until(EC.visibility_of_element_located((By.LINK_TEXT, 'Security'))).click()
     wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'a[tooltip*="Current token(s)"]')))
 
-    tokens_list = main_page.find_elements(By.CSS_SELECTOR, '.token-list-existing-item>input[name="tokenName"]')
+    existing_tokens = main_page.find_elements(By.CSS_SELECTOR, '.token-list-existing-item > input[name="tokenName"]')
 
-    if len(tokens_list):
-        values = [element.get_attribute('value') for element in tokens_list]
+    if existing_tokens:
+        token_names = [token.get_attribute('value') for token in existing_tokens]
 
-        if any(Freestyle.project_name in value for value in values):
-            revoke_css = (
+        if any(Freestyle.project_name in name for name in token_names):
+            revoke_selector = (
                 By.CSS_SELECTOR,
                 f'input[value="{Freestyle.project_name}"] ~ span.to-right > a[data-target-url*="/revoke"]'
             )
-            project_token_revoke_links = wait.until(EC.visibility_of_all_elements_located(revoke_css))
 
-            if project_token_revoke_links:
-                for link in project_token_revoke_links:
-                    wait.until(EC.element_to_be_clickable(link)).click()
-                    wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[data-id='ok']"))).click()
+            revoke_links = wait.until(EC.visibility_of_all_elements_located(revoke_selector))
+
+            for link in revoke_links:
+                wait.until(EC.element_to_be_clickable(link)).click()
+                wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[data-id='ok']"))).click()
 
 
 @pytest.fixture(scope="function")
 def auth_token(main_page, config):
+    """
+    Fixture to generate a new authentication token for the current project
+    and return its value for use in tests.
+    """
     wait = WebDriverWait(main_page, 10)
 
     wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button.repeatable-add"))).click()
-    (wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR,"input[placeholder='Default name']")))
-     .send_keys(Freestyle.project_name))
+    token_name_input = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR,"input[placeholder='Default name']")))
+    token_name_input.send_keys(Freestyle.project_name)
     wait.until(EC.element_to_be_clickable((By.ID, "api-token-property-token-save"))).click()
+    token_element = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[title='Copy this token'")))
 
-    token = (wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[title='Copy this token'")))
-             .get_attribute("text"))
+    token = token_element.get_attribute("text")
 
     main_page.find_element(By.NAME, "Submit").click()
     wait.until(EC.visibility_of_element_located((By.LINK_TEXT, "Dashboard"))).click()
--- tests/freestyle_project/test_build_triggers.py
+++ tests/freestyle_project/test_build_triggers.py
@@ -1,25 +1,22 @@
 import logging
-from time import sleep
-
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
 from tests.freestyle_project.freestyle_data import Freestyle
 
 logger = logging.getLogger(__name__)
 
 
-def test_user_can_trigger_builds_remotely(revoke_tokens, auth_token, freestyle, config):
+def test_user_can_trigger_builds_remotely(revoke_project_tokens, auth_token, freestyle, config):
     wait10 = WebDriverWait(freestyle, 10)
     wait60 = WebDriverWait(freestyle, 60)
 
     token = auth_token
 
-    trigger_builds_remotely_checkbox = freestyle.find_element(By.CSS_SELECTOR,
-                                                              "input[name='pseudoRemoteTrigger']~label")
-    freestyle.execute_script('arguments[0].scrollIntoView({block: "center", inline: "center"})',
-                             trigger_builds_remotely_checkbox)
-    wait10.until(EC.element_to_be_clickable(trigger_builds_remotely_checkbox)).click()
+    checkbox = freestyle.find_element(By.CSS_SELECTOR, "input[name='pseudoRemoteTrigger']~label")
+    freestyle.execute_script('arguments[0].scrollIntoView({block: "center", inline: "center"})', checkbox)
+    wait10.until(EC.element_to_be_clickable(checkbox)).click()
     wait10.until(EC.visibility_of_element_located((By.NAME, "authToken"))).send_keys(token)
     freestyle.find_element(By.NAME, "Submit").click()
 
@@ -29,54 +26,42 @@ def test_user_can_trigger_builds_remotely(revoke_tokens, auth_token, freestyle, 
     freestyle.get("http://localhost:8080/crumbIssuer/api/json")
 
     crumb = freestyle.find_element(By.CSS_SELECTOR, "body").text.split('"')[7]
-    job_name = Freestyle.project_name.replace(" ", "%20")
+    job_name = Freestyle.encoded_project_name
     username = config.jenkins.USERNAME
     password = config.jenkins.PASSWORD
     server = f"{config.jenkins.HOST}:{config.jenkins.PORT}"
+    base_url = config.jenkins.base_url
 
-    trigger_job_api_1 = f"{config.jenkins.base_url}/job/{job_name}/build?token={token}"
-    trigger_job_api_2 = f"{config.jenkins.base_url}/job/{job_name}/build?token={token}&Jenkins-Crumb={crumb}"
-    trigger_job_api_3 = f"http://{username}:{password}@{server}/job/{job_name}/build?token={token}&Jenkins-Crumb={crumb}"
-
-    # freestyle.switch_to.new_window()
-    # freestyle.get(trigger_job_api_1)
-    # logger.info(f"WINDOW 1: {freestyle.current_url}")
-    # logger.info(f"WINDOW 1: {freestyle.find_element(By.CSS_SELECTOR, "body").text}")
-
-    # freestyle.switch_to.new_window()
-    # freestyle.get(trigger_job_api_2)
-    # logger.info(f"WINDOW 2: {freestyle.current_url}")
-    # logger.info(f"WINDOW 2: {freestyle.find_element(By.CSS_SELECTOR, "body").text}")
+    api_urls = {
+        "api_1": f"{base_url}/job/{job_name}/build?token={token}",
+        "api_2": f"{base_url}/job/{job_name}/build?token={token}&Jenkins-Crumb={crumb}",
+        "api_3": f"http://{username}:{password}@{server}/job/{job_name}/build?token={token}&Jenkins-Crumb={crumb}"
+    }
 
     freestyle.switch_to.new_window()
-    freestyle.get(trigger_job_api_3)
-    freestyle.refresh()
-    logger.info(f"WINDOW 3: {freestyle.current_url}")
-    logger.info(f"WINDOW 3: {freestyle.find_element(By.CSS_SELECTOR, "body").text}")
+    freestyle.get(api_urls["api_1"])
+    # freestyle.refresh()
+    logger.info(f"Triggered build at: {freestyle.current_url}")
+    logger.info(f"Build response: {freestyle.find_element(By.CSS_SELECTOR, 'body').text}")
 
     freestyle.switch_to.window(app_window)
 
     wait10.until(EC.visibility_of_element_located((By.LINK_TEXT, "Dashboard"))).click()
-    logger.info(f"On Dashboard page: {freestyle.current_url}")
+    logger.info(f"Returned to Dashboard: {freestyle.current_url}")
 
     wait10.until(EC.visibility_of_element_located((By.LINK_TEXT, "Build History"))).click()
-    logger.info(f"On Build History page: {freestyle.current_url}")
+    logger.info(f"Viewing Build History: {freestyle.current_url}")
 
-    # wait60.until(EC.text_to_be_present_in_element(
-    #     (By.CSS_SELECTOR, "#buildQueue>.pane-content tbody>tr>td"),
-    #     "No builds in the queue.")
-    # )
-
-    sleep(60)
+    queue_status = (By.CSS_SELECTOR, "#buildQueue .pane-content")
+    wait60.until(EC.text_to_be_present_in_element(queue_status, "No builds in the queue."))
+    logger.info("Waiting for the build to finish...")
 
     freestyle.refresh()
+    logger.info("Build finished. Refreshing status.")
 
-    logger.info(f"After build finished")
+    builds = wait10.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "#projectStatus>tbody>tr")))
 
-    builds_list = wait10.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "#projectStatus>tbody>tr")))
-    logger.info(f"{len(builds_list)}")
-
-    assert len(builds_list) == 1, f"Unexpected numbers of builds created {len(builds_list)}"
+    assert len(builds) == 1, f"Expected 1 build, found {len(builds)}"
     assert freestyle.find_element(By.LINK_TEXT, f"{Freestyle.project_name}").is_displayed(), \
-        f"No builds created for {Freestyle.project_name}"
-    assert freestyle.find_element(By.LINK_TEXT, "#1"), "Unexpected builds enumeration."
+        f"No build entry found for project '{Freestyle.project_name}'"
+    assert freestyle.find_element(By.LINK_TEXT, "#1"), "Build #1 not found."
