85d249b7fb4ae82c3bff454c63ba1f7f78e33d87
Petrichor <180253417+IrinaPetrichor@users.noreply.github.com>
2025-05-01 06:23:43 -0400

    return sleep

--- tests/freestyle_project/test_build_triggers.py
+++ tests/freestyle_project/test_build_triggers.py
@@ -1,4 +1,6 @@
 import logging
+from time import sleep
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -10,7 +12,6 @@ logger = logging.getLogger(__name__)
 
 def test_user_can_trigger_builds_remotely(revoke_project_tokens, auth_token, freestyle, config):
     wait10 = WebDriverWait(freestyle, 10)
-    wait60 = WebDriverWait(freestyle, 60)
 
     token = auth_token
 
@@ -30,19 +31,12 @@ def test_user_can_trigger_builds_remotely(revoke_project_tokens, auth_token, fre
     username = config.jenkins.USERNAME
     password = config.jenkins.PASSWORD
     server = f"{config.jenkins.HOST}:{config.jenkins.PORT}"
-    base_url = config.jenkins.base_url
-
-    api_urls = {
-        "api_1": f"{base_url}/job/{job_name}/build?token={token}",
-        "api_2": f"{base_url}/job/{job_name}/build?token={token}&Jenkins-Crumb={crumb}",
-        "api_3": f"http://{username}:{password}@{server}/job/{job_name}/build?token={token}&Jenkins-Crumb={crumb}"
-    }
+    api_url = f"http://{username}:{password}@{server}/job/{job_name}/build?token={token}&Jenkins-Crumb={crumb}"
 
     freestyle.switch_to.new_window()
-    freestyle.get(api_urls["api_3"])
+    freestyle.get(api_url)
     freestyle.refresh()
     logger.info(f"Triggered build at: {freestyle.current_url}")
-    logger.info(f"Build response: {freestyle.find_element(By.CSS_SELECTOR, 'body').text}")
 
     freestyle.switch_to.window(app_window)
 
@@ -52,12 +46,11 @@ def test_user_can_trigger_builds_remotely(revoke_project_tokens, auth_token, fre
     wait10.until(EC.visibility_of_element_located((By.LINK_TEXT, "Build History"))).click()
     logger.info(f"Viewing Build History: {freestyle.current_url}")
 
-    queue_status = (By.CSS_SELECTOR, "#buildQueue .pane-content")
-    wait60.until(EC.text_to_be_present_in_element(queue_status, "No builds in the queue."))
+    sleep(30)
     logger.info("Waiting for the build to finish...")
 
     freestyle.refresh()
-    logger.info("Build finished. Refreshing status.")
+    logger.info("Refreshing status.")
 
     builds = wait10.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "#projectStatus>tbody>tr")))
 
