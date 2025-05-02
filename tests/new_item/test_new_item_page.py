37a0e8d680c5178b3ac9eb89499da8901e76757b
Polina <thepolinaguller@gmail.com>
2025-05-02 16:47:05 +0300

    Fix test for New Item page

--- tests/new_item/test_new_item_page.py
+++ tests/new_item/test_new_item_page.py
@@ -9,8 +9,6 @@ def test_new_item_page_accessibility(main_page):
     # We expect that the input field will be available
     wait = WebDriverWait(main_page, 10)
     input_field = wait.until(EC.visibility_of_element_located((By.NAME, "name")))
-    print(input_field)
-    print(input_field.get_attribute("outerHTML"))
     # Checking the input field
     assert input_field.is_displayed()
 
@@ -29,5 +27,5 @@ def test_available_project_types_displayed(main_page):
 
     for item in expected_items:
         item_label = wait.until(EC.visibility_of_element_located((By.XPATH, f"//label[contains(., '{item}')]")))
-        print(item_label.text)  # Отладка
+        print(item_label.text)
         assert item_label.is_displayed(), f"Project type '{item}' not displayed"
