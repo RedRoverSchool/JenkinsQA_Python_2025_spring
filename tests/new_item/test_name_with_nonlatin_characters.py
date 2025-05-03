2ee6f9011d9d5767010b7315edaea9e8fa2265c8
Rache777 <glebkomov1996@gmail.com>
2025-05-03 07:46:37 +0200

    Fix: update URL check in test_name_with_nonlatin_characters_url

--- tests/new_item/test_name_with_nonlatin_characters.py
+++ tests/new_item/test_name_with_nonlatin_characters.py
@@ -7,7 +7,8 @@ def test_name_with_nonlatin_characters_url(new_item_page):
     new_item_page.find_element(By.ID, "name").send_keys("狒狒")
     wait5 = WebDriverWait(new_item_page, timeout=5, poll_frequency=0.5)
     wait5.until(EC.element_to_be_clickable((By.ID, "ok-button"))).click()
-    assert new_item_page.current_url == "http://localhost:8080/job/%E7%8B%92%E7%8B%92/configure", f"invalid URL: {new_item_page.current_url}"
+    wait5.until(EC.url_contains("/configure"))
+    assert new_item_page.current_url == "http://localhost:8080/job/%E7%8B%92%E7%8B%92/configure", f"invalid URL"
 
 def test_name_with_with_nonlatin_characters_items(new_item_page):
     new_item_page.find_element(By.XPATH, "/html/body/div[3]/div/div/form/div[2]/div[2]/div[1]/ul/li[1]").click()
