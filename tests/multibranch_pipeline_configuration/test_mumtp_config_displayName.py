3118af83597551dcbffb5e697580babddd77bf5d
lenka0376 <elena.goren@gmail.com>
2025-05-02 21:34:50 -0500

    added scroll

--- tests/multibranch_pipeline_configuration/test_mumtp_config_displayName.py
+++ tests/multibranch_pipeline_configuration/test_mumtp_config_displayName.py
@@ -8,8 +8,11 @@ def test_display_name_exist(main_page):
     wait = WebDriverWait(main_page, 20)
     main_page.find_element(By.XPATH, "//a[@href='/view/all/newJob']").click()
     wait.until(EC.presence_of_element_located((By.ID, 'name'))).send_keys("project-name")
-    time.sleep(3)
-    wait.until(EC.presence_of_element_located((By.CLASS_NAME, "org_jenkinsci_plugins_workflow_multibranch_WorkflowMultiBranchProject"))).click()
+    element = wait.until(EC.presence_of_element_located(
+        (By.CLASS_NAME, "org_jenkinsci_plugins_workflow_multibranch_WorkflowMultiBranchProject")
+    ))
+    main_page.execute_script("arguments[0].scrollIntoView(true);", element)
+    element.click()
     wait.until(EC.element_to_be_clickable((By.ID, "ok-button"))).click()
     display_name = wait.until(EC.presence_of_element_located((By.NAME, '_.displayNameOrNull')))
     assert display_name.is_displayed()
