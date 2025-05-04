9cfc1453ad97ab3d8ed3240b62f865c68ebf74cb
kravlad <kravlad86@gmail.com>
2025-05-03 22:19:54 -0700

    refactor: move some business logic from test to FreestyleProjectConfigPage
    add BUILD_STEPS_LIST locator add get_add_build_step_button method add get_build_steps_list method

--- pages/freestyle_project_config_page.py
+++ pages/freestyle_project_config_page.py
@@ -1,11 +1,27 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from pages.base_page import BasePage
 
 
 class FreestyleProjectConfigPage(BasePage):
     class Locator:
         GENERAL_BUTTON = (By.ID, "general")
+        ADD_BUILD_STEP_BUTTON = (
+            By.XPATH, "//button[@class='jenkins-button hetero-list-add' and @suffix='builder']"
+        )
+        BUILD_STEPS_LIST = (By.XPATH, "//button[@class='jenkins-dropdown__item ']")
 
     def __init__(self, driver, project_name, timeout=5):
         super().__init__(driver, timeout=timeout)
         self.url = self.base_url + f"/job/{project_name}/configure"
+
+    def get_add_build_step_button(self):
+        button = self.find_element(*self.Locator.ADD_BUILD_STEP_BUTTON)
+        return button
+
+    def get_build_steps_list(self):
+        button = self.get_add_build_step_button()
+        button.send_keys(Keys.END)
+        button.click()
+        self.wait_to_be_clickable(self.Locator.BUILD_STEPS_LIST)
+        return self.find_elements(*self.Locator.BUILD_STEPS_LIST)
