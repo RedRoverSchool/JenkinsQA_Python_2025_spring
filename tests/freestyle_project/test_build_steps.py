61cbd3afb561cf9aaf6c7b8b80e5c7dc2abdf9d0
kravlad <kravlad86@gmail.com>
2025-05-03 22:36:32 -0700

    refactor: rename freestyle_project_config_page to page

--- tests/freestyle_project/test_build_steps.py
+++ tests/freestyle_project/test_build_steps.py
@@ -3,10 +3,10 @@ from tests.new_item.data import new_freestyle_project_name, build_steps_list
 
 
 def test_build_steps_availability(new_item_page: NewItemPage):
-    freestyle_project_config_page = new_item_page.create_new_freestyle_project(
+    page = new_item_page.create_new_freestyle_project(
         new_freestyle_project_name
     )
-    build_steps_we = freestyle_project_config_page.get_build_steps_list()
+    build_steps_we = page.get_build_steps_list()
     build_steps_items = {el.text for el in build_steps_we}
 
     assert build_steps_list == build_steps_items
