68e09278a088d9e44518f056b5767996ca48aef6
kravlad <kravlad86@gmail.com>
2025-05-03 22:15:36 -0700

    refactor: move build steps list from test to data.py

--- tests/new_item/data.py
+++ tests/new_item/data.py
@@ -1,2 +1,11 @@
-new_folder_name = 'Test_Folder'
-new_freestyle_project_name = 'Test_Freestyle_Project'
+new_folder_name = "Test_Folder"
+new_freestyle_project_name = "Test_Freestyle_Project"
+build_steps_list = {
+    "Execute Windows batch command",
+    "Execute shell",
+    "Invoke Ant",
+    "Invoke Gradle script",
+    "Invoke top-level Maven targets",
+    "Run with timeout",
+    'Set build status to "pending" on GitHub commit',
+}
