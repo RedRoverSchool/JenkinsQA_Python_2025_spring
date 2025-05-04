a8f1f8b8ec5ef565621bd15e9cbf12ea26e69790
kravlad <kravlad86@gmail.com>
2025-05-03 22:43:39 -0700

    refactor: rename  NewItemPage locators FOLDER_BUTTON and FREESTYLE_PROJECT_BUTTON to ITEM_FOLDER and ITEM_FREESTYLE_PROJECT respectively

--- pages/new_item_page.py
+++ pages/new_item_page.py
@@ -5,8 +5,8 @@ from pages.base_page import BasePage
 class NewItemPage(BasePage):
     class Locator:
         ITEM_NAME = (By.CSS_SELECTOR, '#name')
-        FOLDER_BUTTON = (By.CSS_SELECTOR, '[class*="cloudbees_hudson_plugins_folder"]')
-        FREESTYLE_PROJECT_BUTTON = (By.CSS_SELECTOR, '[class*="hudson_model_FreeStyleProject"]')
+        ITEM_FOLDER = (By.CSS_SELECTOR, '[class*="cloudbees_hudson_plugins_folder"]')
+        ITEM_FREESTYLE_PROJECT = (By.CSS_SELECTOR, '[class*="hudson_model_FreeStyleProject"]')
         OK_BUTTON = (By.CSS_SELECTOR, '#ok-button')
 
     def __init__(self, driver, timeout=5):
@@ -16,13 +16,13 @@ class NewItemPage(BasePage):
     def create_new_folder(self, name):
         from pages.folder_config_page import FolderConfigPage
         self.wait_for_element(self.Locator.ITEM_NAME).send_keys(name)
-        self.wait_to_be_clickable(self.Locator.FOLDER_BUTTON).click()
+        self.wait_to_be_clickable(self.Locator.ITEM_FOLDER).click()
         self.wait_to_be_clickable(self.Locator.OK_BUTTON).click()
         return FolderConfigPage(self.driver, name).wait_for_url()
 
     def create_new_freestyle_project(self, name):
         from pages.freestyle_project_config_page import FreestyleProjectConfigPage
         self.wait_for_element(self.Locator.ITEM_NAME).send_keys(name)
-        self.wait_to_be_clickable(self.Locator.FREESTYLE_PROJECT_BUTTON).click()
+        self.wait_to_be_clickable(self.Locator.ITEM_FREESTYLE_PROJECT).click()
         self.wait_to_be_clickable(self.Locator.OK_BUTTON).click()
         return FreestyleProjectConfigPage(self.driver, name).wait_for_url()
