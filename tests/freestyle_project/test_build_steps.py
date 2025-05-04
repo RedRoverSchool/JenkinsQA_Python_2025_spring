c3a3fa79c035c59dd1e937bcbb865222bb0a5a3d
kravlad <kravlad86@gmail.com>
2025-05-03 22:21:48 -0700

    refactor: move some business logic to FreestyleProjectConfigPage

--- tests/freestyle_project/test_build_steps.py
+++ tests/freestyle_project/test_build_steps.py
@@ -1,35 +1,12 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from pages.new_item_page import NewItemPage
-from tests.new_item.data import new_freestyle_project_name
+from tests.new_item.data import new_freestyle_project_name, build_steps_list
 
 
 def test_build_steps_availability(new_item_page: NewItemPage):
     freestyle_project_config_page = new_item_page.create_new_freestyle_project(
         new_freestyle_project_name
     )
-    freestyle_project_config_page.wait_for_url()
-
-    add_build_step_button = freestyle_project_config_page.find_element(
-        By.XPATH,
-        "//button[@class='jenkins-button hetero-list-add' and @suffix='builder']",
-    )
-    add_build_step_button.send_keys(Keys.END)
-    add_build_step_button.click()
-
-    build_steps_locator = (By.XPATH, "//button[@class='jenkins-dropdown__item ']")
-    freestyle_project_config_page.wait_to_be_clickable(build_steps_locator)
-    build_steps_we = freestyle_project_config_page.find_elements(*build_steps_locator)
-
+    build_steps_we = freestyle_project_config_page.get_build_steps_list()
     build_steps_items = {el.text for el in build_steps_we}
-    expected_items = {
-        "Execute Windows batch command",
-        "Execute shell",
-        "Invoke Ant",
-        "Invoke Gradle script",
-        "Invoke top-level Maven targets",
-        "Run with timeout",
-        'Set build status to "pending" on GitHub commit',
-    }
 
-    assert expected_items == build_steps_items
+    assert build_steps_list == build_steps_items
