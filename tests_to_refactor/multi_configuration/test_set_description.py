bebe44ed08b8b0275bb0394c26ff69575c9a476b
Liliana <lilianakoric@gmail.com>
2025-05-06 19:52:12 -0700

    Refactor test to POM

--- tests_to_refactor/multi_configuration/test_set_description.py
+++
@@ -1,9 +0,0 @@
-
-
-def test_set_description_for_multi_config_project(multi_config_project_page):
-    description_text = "This is my overview"
-
-    multi_config_project_page.set_description(description_text)
-    saved_text = multi_config_project_page.get_saved_description_text()
-
-    assert description_text in saved_text, f"Expected '{description_text}', but got '{saved_text}'"
