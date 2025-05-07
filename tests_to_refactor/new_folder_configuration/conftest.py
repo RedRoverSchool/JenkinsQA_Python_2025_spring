d54b146ef8aeebd799d41481dd6b18bad69e1512
Space-researcher <space-researcher@i.ua>
2025-05-07 15:15:55 +0300

    pom for tc_05.001.05

--- tests_to_refactor/new_folder_configuration/conftest.py
+++ tests_to_refactor/new_folder_configuration/conftest.py
@@ -1,16 +1,9 @@
 import pytest
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from tests.new_folder_configuration.variables import FolderNames
 
-@pytest.fixture(scope="function")
-def create_folder(main_page):
-    def create_folder_inner(item_name):
-        wait = WebDriverWait(main_page, 10)
-        # Create a folder
-        wait.until(EC.visibility_of_element_located((By.XPATH, "//a[@href='newJob']"))).click()
-        main_page.find_element(By.CLASS_NAME, "jenkins-input").send_keys(item_name)
-        main_page.find_element(By.CLASS_NAME, "com_cloudbees_hudson_plugins_folder_Folder").click()
-        main_page.find_element(By.ID, "ok-button").click()
-    return create_folder_inner
+@pytest.fixture
+def folder_config_page(driver, main_page):
+    folder_name = FolderNames.item_name
+    page = main_page.go_to_new_item_page().create_new_folder(folder_name)
+    return page
 
