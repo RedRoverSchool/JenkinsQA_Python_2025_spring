f628aeadc588f9e7fa1918d5704b718ab7fc1cc1
Zarina <zaya110589@gmail.com>
2025-05-07 23:40:57 -0400

    refactored test_copy_from_item.py

--- pages/new_item_page.py
+++ pages/new_item_page.py
@@ -13,7 +13,6 @@ class NewItemPage(BasePage):
         ERROR_MESSAGE = (By.ID, "itemname-required")
         ITEM_MULTI_CONFIG_PROJECT = (By.CLASS_NAME, "hudson_matrix_MatrixProject")
         ITEM_TYPES = (By.CSS_SELECTOR, ".label")
-        NEW_ITEM = (By.XPATH, "//a[@href='/view/all/newJob']")
         COPY_FROM = (By.CSS_SELECTOR, "input.jenkins-input.auto-complete")
 
     def __init__(self, driver, timeout=5):
@@ -68,9 +67,6 @@ class NewItemPage(BasePage):
     def get_item_types_text(self):
         elements = self.wait_to_be_visible_all(self.Locator.ITEM_TYPES)
         return [element.text for element in elements]
-
-    def click_new_item_button(self):
-        return self.wait_to_be_clickable(self.Locator.NEW_ITEM).click()
 
     def copy_from_option_is_displayed(self):
         return self.wait_to_be_visible(self.Locator.COPY_FROM).is_displayed()
