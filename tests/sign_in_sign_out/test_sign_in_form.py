425d90c46babcdcd31a5dcf42476270c7b538350
Anastasiya Kuzniatsova <anastasiyasmithk@gmail.com>
2025-05-07 22:20:15 -0700

    Rename file name and test name

--- tests/sign_in_sign_out/test_sign_in_form.py
+++ tests/sign_in_sign_out/test_sign_in_form.py
@@ -2,7 +2,7 @@ from pages.login_page import LoginPage
 from tests.sign_in_sign_out.data import sign_in_form_header
 
 
-def test_signin_fields(login_page: LoginPage):
+def test_verify_signin_form(login_page: LoginPage):
     actual_sign_in_form_header = login_page.get_sign_in_form_header()
 
     assert actual_sign_in_form_header == sign_in_form_header, f"expected: '{sign_in_form_header}', actual: '{actual_sign_in_form_header}'"
