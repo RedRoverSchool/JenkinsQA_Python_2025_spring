7758d8a342204cba0d01d252b0db86a67a5802a7
yaroslavmilkevych <yaroslav08101991@gmail.com>
2025-05-09 17:39:24 +0200

    fixture added (#214)
    * fixture addeddd
    
    * Update test_herokuapp.py

--- tests/new_item/test_the selected item type.py
+++ tests/new_item/test_the selected item type.py
@@ -1,21 +1,11 @@
-import pytest
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
-import time
+def test_the_selected_item_type(new_item_page):
+        freestyle_element = new_item_page.get_freestyle_element()
+        freestyle_element.click()
 
+        selected_items = new_item_page.get_selected_items()
 
+        assert len(selected_items) == 1
+        assert selected_items[0] == freestyle_element
 
-def test_item_type_selection_sets_active_class(new_item_page):
-        wait5 = WebDriverWait(new_item_page, 5)
-        selected_type = wait5.until(
-                EC.element_to_be_clickable((By.XPATH, "//li[contains(@class, 'hudson_model_FreeStyleProject')]"))
-        )
-        selected_type.click()
-
-        items = new_item_page.find_elements(By.CSS_SELECTOR, ".j-item-options li")
-        assert selected_type.get_attribute("aria-checked") == "true" and not any(
-                item != selected_type and item.get_attribute("aria-checked") == "true"
-                for item in items
-        )
-
+        highlighted = new_item_page.get_highlighted_items()
+        assert freestyle_element in highlighted
