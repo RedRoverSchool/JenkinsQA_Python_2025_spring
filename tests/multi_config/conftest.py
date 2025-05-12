01ea783d3946234ec1b28eee4cff49b64dc8612e
Liliana <lilianakoric@gmail.com>
2025-05-12 08:50:57 -0700

    Add edit_description test with updated Page Object and fixture

--- tests/multi_config/conftest.py
+++ tests/multi_config/conftest.py
@@ -1,7 +1,16 @@
 import pytest
 from tests.multi_config.data import project_name
+from dotenv import load_dotenv
+load_dotenv()
 
 
 @pytest.fixture
 def multi_config_project_config_page(new_item_page):
     return new_item_page.create_new_multi_config_project(project_name)
+
+
+@pytest.fixture
+def multi_config_project_with_description(new_item_page):
+    from tests.multi_config.data import project_name, description_text
+    config_page = new_item_page.create_new_multi_config_project(project_name)
+    return config_page.set_description(description_text, project_name)
