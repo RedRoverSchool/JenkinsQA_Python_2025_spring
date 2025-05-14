f50de8907a771b93896bce6a0bfe0e8fcca903bc
ppashchenkov <email@example.com>
2025-05-14 16:09:09 +0300

    Added test_sign_out_windows.py - TC_08.002.01 | Sign in/out > Sign out

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,6 +1,6 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
-from selenium.webdriver.remote.webelement import WebElement
+
 
 from pages.base_page import BasePage
 from core.jenkins_utils import update_crumb
