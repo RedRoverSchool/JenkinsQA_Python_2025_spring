f16b0fb25fec665c622eabdca49507f5e2c74707
dsmirnov <mitsmirnov@gmail.com>
2025-05-16 01:22:15 -0230

    Refactoring
    # Conflicts:
    #	pages/base_page.py
    #	pages/main_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,9 @@
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 
 from pages.ui_element import UIElementMixin
 from pages.components.components import Header
@@ -10,6 +12,7 @@ from pages.components.components import Header
 class BasePage(UIElementMixin):
     def __init__(self, driver: WebDriver, timeout = 5):
         super().__init__(driver)
+        self.timeout = timeout
         self.base_url = self.config.jenkins.base_url
         self.header = Header(driver)
 
@@ -24,15 +27,26 @@ class BasePage(UIElementMixin):
             self.logger.error(f"Timeout when waiting for url {self.url}, current url: {self.driver.current_url}")
         return self
 
+
     def get_title(self) -> str:
         return self.driver.title
 
-    def get_current_window_handle(self):
-        return self.driver.current_window_handle
+    def is_element_present(self, by, value):
+        try:
+            self.driver.find_element(by, value)
+            return True
+        except NoSuchElementException:
+            return False
 
-    def get_all_windows_handles(self):
-        return self.driver.window_handles
+    def get_header_text(self):
+        header = WebDriverWait(self.driver, self.timeout).until(
+            EC.visibility_of_element_located((By.TAG_NAME, "h1"))
+        )
+        return header.text
 
-    def switch_to_window(self, handle):
-        self.driver.switch_to.window(handle)
-        return self.driver
+    def is_header_contains(self, text):
+        return text in self.get_header_text()
+
+    def get_text(self, locator, timeout=10):
+        element = self.wait_for_element(locator, timeout)
+        return element.text
