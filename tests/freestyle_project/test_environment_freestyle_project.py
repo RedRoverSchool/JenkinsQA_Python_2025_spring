20642f125fe7289d21d3b395948372001f243a15
ppashchenkov <email@example.com>
2025-05-22 14:29:02 +0300

    Refactoring test_environment_freestyle_project.py to add allure reports

--- tests/freestyle_project/test_environment_freestyle_project.py
+++ tests/freestyle_project/test_environment_freestyle_project.py
@@ -14,7 +14,7 @@ def test_exist_environment_section(freestyle):
 
 @allure.epic("Freestyle Project Configuration")
 @allure.story("Set up Environment")
-@allure.title("\"Environment\" section should be available, helper tooltip appears, empty environment is permissible")
+@allure.title("The helper tooltip appears when the mouse cursor hovers over the question marks")
 @allure.testcase("TC_02.005.01")
 @allure.link("https://github.com/RedRoverSchool/JenkinsQA_Python_2025_spring/issues/407", name="Github issue")
 @pytest.mark.parametrize('tp_link, tp_expected_text', Freestyle.tooltip_environment)
