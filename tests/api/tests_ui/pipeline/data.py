214a59e093c4ce64b9c211d98dbd4d13065666f6
Petrichor <180253417+IrinaPetrichor@users.noreply.github.com>
2025-06-10 19:30:24 -0400

    Add preconditions by API

---
+++ tests/api/tests_ui/pipeline/data.py
@@ -0,0 +1,119 @@
+import allure
+from tests.api.utils.helpers import create_unique_name
+
+
+class BuildsCounter:
+    builds_history_limit_30 = 30
+    builds_history_limit_31 = 31
+    builds_history_limit_60 = 60
+    builds_history_limit_61 = 61
+    builds_history_page_limit = 30
+    builds_to_keep_70 = 70
+    amount_to_wait = 5
+
+
+class Script:
+    two_stages_with_post = """pipeline {
+    agent any
+    stages {
+        stage('Stage 1 Test') {
+            steps {
+                script {
+                    if (isUnix()) {
+                        sh 'echo Step 1'
+                        sh 'exit 0'
+                    } else {
+                        bat 'echo Step 1'
+                        bat 'exit 0'
+                    }
+                }
+            }
+        }
+        stage('Stage 2 Message') {
+            steps {
+                script {
+                    if (isUnix()) {
+                        sh 'echo Step 2'
+                        sh 'echo Success!'
+                    } else {
+                        bat 'echo Step 2'
+                        bat 'echo Success!'
+                    }
+                }
+            }
+        }
+    }
+    post {
+        always {
+            echo 'This will always run'
+        }
+        success {
+            echo 'This will run only if successful'
+        }
+        failure {
+            echo 'This will run only if failed'
+        }
+        unstable {
+            echo 'This will run only if marked as unstable'
+        }
+        changed {
+            echo 'This will run only if changed'
+        }
+    }
+}
+    """
+
+    script_default = two_stages_with_post
+
+
+class Description:
+    description = "Created via Jenkins API."
+    triggers_default = " Triggers builds remotely."
+    builds_to_keep_default = BuildsCounter.builds_to_keep_70
+
+    @classmethod
+    def get_pipeline_scripted_keep_70_builds_remotely_description(cls):
+        return f"{cls.description} {cls.builds_to_keep_default} builds to keep.{cls.triggers_default}"
+
+
+class Config:
+    @classmethod
+    def get_pipeline_scripted_keep_70_builds_remotely_xml(
+            cls,
+            description: str=Description.description,
+            script: str=Script.script_default,
+            token: str = None,
+            log_rotator: int=BuildsCounter.builds_to_keep_70
+    ) -> str:
+        return f"""
+            <flow-definition plugin="workflow-job">
+                <description>{description}</description>
+                <keepDependencies>false</keepDependencies>
+                <logRotator class="hudson.tasks.LogRotator">
+                    <daysToKeep>-1</daysToKeep>
+                    <numToKeep>{log_rotator}</numToKeep>
+                    <artifactDaysToKeep>-1</artifactDaysToKeep>
+                    <artifactNumToKeep>-1</artifactNumToKeep>
+                </logRotator>
+                <definition class="org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition" plugin="workflow-cps">
+                    <script>{script}</script>
+                    <sandbox>true</sandbox>
+                </definition>
+                <triggers/>
+                <authToken>{token}</authToken>
+                <disabled>false</disabled>
+            </flow-definition>
+            """
+
+
+class Data:
+    @classmethod
+    def get_pipeline_scripted_keep_70_data(cls, token: str):
+        item_name = create_unique_name(prefix="pipeline-api")
+        description = Description.get_pipeline_scripted_keep_70_builds_remotely_description()
+        log_rotator = BuildsCounter.builds_to_keep_70
+        script = Script.two_stages_with_post
+        config_xml = Config.get_pipeline_scripted_keep_70_builds_remotely_xml(description, script, token, log_rotator)
+
+        with allure.step(f"Create data for Pipeline project \"{item_name}\", with {log_rotator} builds to keep. Triggers builds remotely."):
+            return item_name, log_rotator, script, config_xml
--- tests/api/tests_ui/pipeline/test_builds_history_pagination.py
+++ tests/api/tests_ui/pipeline/test_builds_history_pagination.py
@@ -1,50 +1,26 @@
 import logging
+import allure
 
 from pages.pipeline_page import PipelinePage
-from tests.pipeline.pipeline_data import BuildCounter
+from tests.api.tests_ui.pipeline.data import BuildsCounter
 
 logger = logging.getLogger(__name__)
 
-
-def test_pipeline_builds_history_pagination_31(create_builds):
-    total_builds = BuildCounter.build_history_limit_31
-    limit_per_page = BuildCounter.build_history_page_limit
-    amount_to_wait = BuildCounter.amount_to_wait
+@allure.epic("Pipeline Management")
+@allure.story("View Build History monitor")
+@allure.title("Paginate builds when count exceeds 30")
+@allure.description("Verify that when the number of builds exceeds 30, pagination controls are displayed \
+                    and allow the user to navigate through the build history.")
+@allure.testcase("https://github.com/RedRoverSchool/JenkinsQA_Python_2025_spring/issues/868", "TC_19.009.01")
+@allure.link("https://github.com/RedRoverSchool/JenkinsQA_Python_2025_spring/issues/868", name="Github issue")
+def test_builds_history_pagination_31(trigger_builds_31, main_page):
+    total_builds = BuildsCounter.builds_history_limit_31
+    limit_per_page = BuildsCounter.builds_history_page_limit
     expected_builds_on_second_page = total_builds - limit_per_page
+    expected_build_number_on_the_second_page = "#1"
 
-    logger.info(f"Creating {total_builds} build(s) with page limit {limit_per_page}, wait for each {amount_to_wait} builds...")
-    pipeline_page: PipelinePage = create_builds(total=total_builds, start=1, amount_to_wait=amount_to_wait)
-
-    builds_on_first_page = len(pipeline_page.get_builds_list())
-
-    assert builds_on_first_page == limit_per_page, \
-        f"Expected {limit_per_page} builds on the first page, but got {builds_on_first_page}."
-    assert pipeline_page.get_next_page_button().is_displayed(), "Next page button is not displayed when it should be."
-    assert pipeline_page.get_next_page_button().is_enabled(), "Next page button is not enabled when it should be."
-    assert pipeline_page.get_previous_page_button().is_displayed(), \
-        "Previous page button is not displayed when it should be."
-    assert pipeline_page.get_previous_page_button().get_attribute("class").endswith("disabled"), \
-        "Previous page button does not have a 'disabled' class as expected."
-
-    builds_on_second_page = len(pipeline_page.click_next_page_button().scroll_to_the_top_of_builds_list().get_builds_list())
-
-    assert builds_on_second_page == expected_builds_on_second_page, \
-        f"Expected {expected_builds_on_second_page} builds on the second page, but got {builds_on_second_page}."
-    assert pipeline_page.get_next_page_button().is_displayed(), "Next page button is not displayed when it should be."
-    assert pipeline_page.get_next_page_button().get_attribute("class").endswith("disabled"), \
-        "Next page button does not have a 'disabled' class as expected."
-    assert pipeline_page.get_previous_page_button().is_displayed(), \
-        "Previous page button is not displayed when it should be."
-    assert pipeline_page.get_previous_page_button().is_enabled(), \
-        "Previous page button is not enabled when it should be."
-
-
-def test_pipeline_builds_history_pagination_31_api(trigger_builds):
-    total_builds = BuildCounter.build_history_limit_31
-    limit_per_page = BuildCounter.build_history_page_limit
-    expected_builds_on_second_page = total_builds - limit_per_page
-
-    pipeline_page = trigger_builds
+    pipeline_name = trigger_builds_31
+    pipeline_page: PipelinePage = main_page.go_to_pipeline_page(pipeline_name)
 
     builds_on_first_page = len(pipeline_page.get_builds_list())
 
@@ -58,9 +34,11 @@ def test_pipeline_builds_history_pagination_31_api(trigger_builds):
         "Previous page button does not have a 'disabled' class as expected."
 
     builds_on_second_page = len(pipeline_page.click_next_page_button().get_builds_list())
+    build_number = pipeline_page.get_single_build_number()
 
     assert builds_on_second_page == expected_builds_on_second_page, \
         f"Expected {expected_builds_on_second_page} builds on the second page, but got {builds_on_second_page}."
+    assert build_number == expected_build_number_on_the_second_page
     assert pipeline_page.get_next_page_button().is_displayed(), "Next page button is not displayed when it should be."
     assert pipeline_page.get_next_page_button().get_attribute("class").endswith("disabled"), \
         "Next page button does not have a 'disabled' class as expected."
@@ -68,3 +46,14 @@ def test_pipeline_builds_history_pagination_31_api(trigger_builds):
         "Previous page button is not displayed when it should be."
     assert pipeline_page.get_previous_page_button().is_enabled(), \
         "Previous page button is not enabled when it should be."
+
+    builds_on_the_first_page_when_return = len(pipeline_page.click_previous_page_button().get_builds_list())
+
+    assert builds_on_the_first_page_when_return == limit_per_page, \
+        f"Expected {limit_per_page} builds on the first page, but got {builds_on_first_page}."
+    assert pipeline_page.get_next_page_button().is_displayed(), "Next page button is not displayed when it should be."
+    assert pipeline_page.get_next_page_button().is_enabled(), "Next page button is not enabled when it should be."
+    assert pipeline_page.get_previous_page_button().is_displayed(), \
+        "Previous page button is not displayed when it should be."
+    assert pipeline_page.get_previous_page_button().get_attribute("class").endswith("disabled"), \
+        "Previous page button does not have a 'disabled' class as expected."
