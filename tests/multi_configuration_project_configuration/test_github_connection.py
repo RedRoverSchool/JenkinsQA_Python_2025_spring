3e2e2bcfe35da51f8d6ac344b3658916d794f430
konsaf <knsfonov@gmail.com>
2025-07-08 22:43:29 -0400

    fix skip

--- tests/multi_configuration_project_configuration/test_github_connection.py
+++ tests/multi_configuration_project_configuration/test_github_connection.py
@@ -4,7 +4,7 @@ import pytest
 from tests.multi_configuration_project_configuration.data import GitHubConnection
 
 
-@pytest.skip("failed on CI")
+@pytest.mark.skip("failed on CI")
 def test_github_connection_error(multi_config_project_with_invalid_github_link):
     project_page = multi_config_project_with_invalid_github_link
     error_text = project_page.go_to_configure_page().click_source_code_management_button().get_invalid_github_link_error_text()
